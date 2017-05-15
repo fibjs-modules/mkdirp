5f4a5fbbbf7c6d6508fbb25c72856801fb416625
unknown <>
2017-05-15 22:46:20 +0800

    fix: test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,10 @@ const test = require("test");
 const fs = require('fs');
 const path = require('path');
 
+const tmp = path.join(__dirname, 'tmp');
+if (!fs.exists(tmp))
+  fs.mkdir(tmp);
+
 const filelist = fs.readdir(__dirname).toJSON();
 filelist.map(file => {
   if (file.endsWith('.test.js')) {
--- test/root.test.js
+++ test/root.test.js
@@ -6,16 +6,16 @@ const test = require("test");
 const _0755 = parseInt('0755', 8);
 
 test.setup();
+if (process.platform !== 'win32') {
+  describe('root', () => {
+    it("root", () => {
+      // '/' on unix, 'c:/' on windows.
+      const file = path.resolve('/');
 
-describe('root', () => {
-  it("root", () => {
-    // '/' on unix, 'c:/' on windows.
-    const file = path.resolve('/');
-
-    mkdirp(file, _0755);
-    const stat = fs.stat(file);
-    assert.ok(stat.isDirectory(), 'target is a directory');
+      mkdirp(file, _0755);
+      const stat = fs.stat(file);
+      assert.ok(stat.isDirectory(), 'target is a directory');
+    });
   });
-});
-
+}
 //test.run(console.DEBUG);
